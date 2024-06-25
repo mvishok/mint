c6298521789cc3b67a28124e7f20e6d5da9d9e78
mvishok <vishokmanikantan@gmail.com>
2024-06-25 10:16:53 +0530

    Using created interpreter class in main.java

--- src/mint.java
+++ src/mint.java
@@ -1,5 +1,11 @@
+import interpreter.*;
+import java.util.List;
+
 public class mint {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Lexer lexer = new Lexer("print\"my world\" 123 true\nif condition then\nprint \"inside if\"\nelse\nprint \"inside else\"\nend\nrepeat 3 times\nprint \"repeat body\"\nend\n");
+        List<String> tokens = lexer.tokenize();
+        Executor executor = new Executor(tokens);
+        executor.execute();
     }
 }
