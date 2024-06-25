92bb1e6c10826c2a6de8891eb6ad53ffec13ac92
mvishok <vishokmanikantan@gmail.com>
2024-06-25 18:42:11 +0530

    feat: Add print method to fns class for printing a list of strings

---
+++ src/functions/fns.java
@@ -0,0 +1,15 @@
+package functions;
+
+import java.util.List;
+
+public class fns {
+
+    public void print(List<String> args) {
+        for (String arg : args) {
+            System.out.print(arg + " ");
+        }
+        System.out.println();
+    }
+
+    
+}
